ec70cd8315cee91a77f32b9aedc78061c5f7c891
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-07-30 11:07:15 -0400

    testing API retrieval

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,37 @@
 import React from 'react'
+import axios from 'axios'
+
 
 class Home extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            info: [],
+            info2: [],
+        }
+    }
+
+    async getAnswer () {
+        try {
+        const response = await axios.get('https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?p=Danny%20Welbeck')
+        console.log(response.data)
+        this.setState({info: response.data}); 
+        this.setState({info2: this.state.info.player[0]})
+
+    } catch {
+
+    }
+}
+
+componentDidMount() {
+    this.getAnswer();
+}
+    
     render() {
         return (
             <div>
-                
+                {this.state.info2.strPlayer}
             </div>
         )
     }
