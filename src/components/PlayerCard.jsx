66bb3581ad280036b060b29f04744ed09a1c7523
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-07-31 13:29:17 -0400

    updated PlayerCard.jsx

--- src/components/PlayerCard.jsx
+++ src/components/PlayerCard.jsx
@@ -3,7 +3,16 @@ import React from 'react'
 function PlayerCard(props) {
     return (
         <div>
-            
+            <h1> {/* Name */} </h1>
+            <h2> Team: {/* Team Name */} </h2>
+            <h3> # {/* Number */} </h3>
+            <h3> Position: {/* Position */} </h3>
+            <p> Height: {/* Height */}kg </p>
+            <p> Weight: {/* Weight */} </p>
+            <p> DOB: {/* DOB */} </p>
+            <p> Status: {/* Signing */} </p>
+            <p> Bio: {/* Description */} </p>
+            <p></p>
         </div>
     )
 }
