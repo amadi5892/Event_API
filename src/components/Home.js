b3151518e1e28b5d3a73a0624b41c7f844ef9677
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-08-02 20:58:39 -0400

    added player of the week to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,10 @@ export default class Home extends React.Component {
                     <h5>Welcome to the Show Down!</h5>
                     <p>Millions across the world come here to find their current or new favorite sports league to watch. Use our country search engine to find out what sports your country has to offer.</p>
                     <iframe className="vid" width="560" height="315" src="https://www.youtube.com/embed/KSddT4yIADE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <div>
+                        <h3>Player of the Week!</h3>
+                    </div>
+
                 </div>
             </div>
         )
