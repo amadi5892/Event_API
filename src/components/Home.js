170e2d05eb4caf6a90b360d12d3556d1fe759b6b
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-08-02 20:44:23 -0400

    updated home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,10 @@ export default class Home extends React.Component {
         return (
             <div>
                 <h1 className="hme-ttl">Show Down</h1>
+                <div className="txt-box">
+                    <h5>Welcome to the Show Down!</h5>
+                    <p>Millions across the world come here to find their current or new favorite sports league to watch. Use our country search engine to find out what sports your country has to offer.</p>
+                </div>
             </div>
         )
     }
