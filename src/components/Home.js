eb1fc105b9b966a5f0540504bbfe40cc1473210d
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-08-02 21:01:20 -0400

    added video for player of the week

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,8 +9,9 @@ export default class Home extends React.Component {
                     <h5>Welcome to the Show Down!</h5>
                     <p>Millions across the world come here to find their current or new favorite sports league to watch. Use our country search engine to find out what sports your country has to offer.</p>
                     <iframe className="vid" width="560" height="315" src="https://www.youtube.com/embed/KSddT4yIADE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    <div>
+                    <div className="ply-week">
                         <h3>Player of the Week!</h3>
+                        <iframe width="560" height="315" src="https://www.youtube.com/embed/MEWSjzw2jVw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </div>
 
                 </div>
