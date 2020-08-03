26d7020f17288c6c917cf1fc331fa061b7a56a33
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-08-02 20:53:48 -0400

    embedded video to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ export default class Home extends React.Component {
                 <div className="txt-box">
                     <h5>Welcome to the Show Down!</h5>
                     <p>Millions across the world come here to find their current or new favorite sports league to watch. Use our country search engine to find out what sports your country has to offer.</p>
+                    <iframe className="vid" width="560" height="315" src="https://www.youtube.com/embed/KSddT4yIADE" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 </div>
             </div>
         )
